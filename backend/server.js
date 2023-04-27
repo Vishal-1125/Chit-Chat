96492ee5472315aa753e53ec4abcc487ad625638
Vishal-1125 <84090714+Vishal-1125@users.noreply.github.com>
2023-04-28 02:32:33 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://chit-chat-taq0.onrender.com",
+    origin: "*",
   },
 });
 
