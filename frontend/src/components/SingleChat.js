7c30fff945e8934f987ff3c42acd5024357893cf
Vishal-1125 <84090714+Vishal-1125@users.noreply.github.com>
2023-04-28 02:20:45 +0530

    update endpoint

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -15,7 +15,7 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "https://chit-chat-8viy.onrender.com/";
+const ENDPOINT = "https://chit-chat-taq0.onrender.com";
 var socket, selectedChatCompare;
 
 const SingleChat = () => {
